35a83e68f4f36ea0556bbe4e4a844e71cb3d6ce0
Kevin McAnoy <kevinmcanoy@gmail.com>
2020-05-12 19:55:11 -0700

    gitlab-cleanup

--- charts/gitlab-delete-stale-projects/gitlab-cleanup.py
+++ charts/gitlab-delete-stale-projects/gitlab-cleanup.py
@@ -15,7 +15,7 @@ git_base_url = os.environ.get('GITLAB_API_URL')
 git_token = os.environ.get('GIT_TOKEN')
 parent_group = os.environ.get('PARENT_GROUP_ID')
 delete_after_hours_string = os.environ.get('DELETE_AFTER_HOURS')
-delete_after_hours = 2147483647
+delete_after_hours = 2147483647 # default but line above must be set and is enforced
 dry_run = os.environ.get('DRY_RUN').lower() == 'true'
 
 def check_env_vars():
@@ -31,7 +31,6 @@ def check_env_vars():
         raise ValueError('You must set a time period to delete projects (DELETE_AFTER_HOURS)')
     if not delete_after_hours_string.isnumeric():
         raise ValueError('You must set a time period to delete projects (DELETE_AFTER_HOURS)')
-    delete_after_hours = int(delete_after_hours_string)
 
 def get_groups(group_id):
     response = requests.get(
@@ -59,64 +58,65 @@ def get_projects(group_id):
 
     for project in projects:
         logger.debug(project['path_with_namespace'])
-        createTime = datetime.datetime.strptime(project['created_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
-        logger.debug("created " + createTime.strftime("%b %d %Y"))
-        passedTime = currentTime - createTime
+        lastActivity = datetime.datetime.strptime(project['last_activity_at'], '%Y-%m-%dT%H:%M:%S.%fZ')
+        logger.debug(f"Last Activity {lastActivity:%b %d %Y}")
+        passedTime = currentTime - lastActivity
         elapsed_hours = passedTime.total_seconds() / 3600
-        logger.debug("Delete? " +  str(elapsed_hours > delete_after_hours) + " :: Hours " + str(int((elapsed_hours))))
+        logger.debug(f"Delete? { elapsed_hours > delete_after_hours } :: Hours { int(elapsed_hours) }")
         if elapsed_hours < delete_after_hours:
-            logger.debug("Found a recent repo in the residency. Don't delete residency. Not empty " + project['path_with_namespace'])
+            logger.debug(f"Found a recent repo for the engagment. No delete. Not empty { project['namespace']['name']}")
             deleted = False
+        else:
+            logger.info(f"Stale repository found. Last Activity {lastActivity:%b %d %Y}")
 
     return deleted
 
 # end get_projects
 
 def delete_group(group_id):
-    logger.info("deleting group " + str(group_id) + " dry run? " + str(dry_run));
+    logger.info(f"deleting group {group_id} dry run? {dry_run}")
     if not dry_run:
         response = requests.delete(
             git_base_url + '/groups/' + str(group_id),
             headers={"PRIVATE-TOKEN": git_token},
         )
-        logger.info(response.status_code)
+        logger.debug("delete response code {response.status_code}")
         if response.status_code == 202 :
-            logger.warn("deleted residency " + str(group_id));
+            logger.warn(f"deleted engagement {group_id}")
         else:
-            logger.error("Failed to delete residency " + str(group_id) + " code " + str(response.status))
+            logger.error(f"Failed to delete engagement {group_id} code {response.status}")
 
 
 # end delete_group
 
 check_env_vars()
+
 git_base_url += '/api/v4'
 if(dry_run):
    logger.info('In dry-run mode. No deletes will occur') 
 
+delete_after_hours = int(delete_after_hours_string)
 logger.info("Delete projects after %i hours", delete_after_hours)
 
 customers = get_groups(parent_group);
 
 for customer in customers:
-    logger.debug("customer " + str(customer['id']))
-    residencies = get_groups(customer['id'])
+    engagements = get_groups(customer['id'])
 
-    logger.debug(len(residencies))
+    logger.debug(f"Engagements for {customer['name']} = {len(engagements)}")
     counter = 0
-    for residency in residencies:
-        ready_for_delete = get_projects(residency['id'])
+    for engagement in engagements:
+        ready_for_delete = get_projects(engagement['id'])
         
         if(ready_for_delete):
-            delete_group(residency['id'])
+            delete_group(engagement['id'])
             counter += 1
     
-    if(counter == len(residencies)):
-        logger.info("All residencies were deleted for customer " + customer['name'])
+    if(counter == len(engagements)):
+        logger.info(f"All engagements were deleted for customer {customer['name']}")
         delete_group(customer['id'])
 
 logger.info("Gitlab clean up complete")
-#"2020-04-15T22:52:10.606Z"
-#"%Y-%m-%dT%H:%M:%S.fZ"
 
 
 
